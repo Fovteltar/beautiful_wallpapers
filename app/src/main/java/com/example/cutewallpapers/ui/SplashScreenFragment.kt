f939a867c05c3686cab1638ec82aef78eff0f24b
Fovteltar <nikitindima002@gmail.com>
2023-01-31 14:59:32 +0300

    Added Splash Screen

---
+++ app/src/main/java/com/example/cutewallpapers/ui/SplashScreenFragment.kt
@@ -0,0 +1,44 @@
+package com.example.cutewallpapers.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.cutewallpapers.databinding.FragmentSplashScreenBinding
+
+class SplashScreenFragment: Fragment() {
+    private var _binding: FragmentSplashScreenBinding? = null
+    val binding get() = checkNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+
+        Handler().postDelayed(
+            Runnable{
+                navController.navigate(
+                    SplashScreenFragmentDirections
+                        .actionSplashScreenFragmentToWallpapersCategoriesFragment()
+                )
+            }, 500)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
